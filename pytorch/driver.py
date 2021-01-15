e57a300c733aceff90e1ad42748755c7444b393e
rahulkrishnan98 <mrahul.krishnan@gmail.com>
2021-01-15 15:27:04 -0500

    updated logging to tensorboard

--- pytorch/driver.py
+++ pytorch/driver.py
@@ -16,8 +16,9 @@ import model as Model
 
 def train(args, device, writer):
     """ Training BiLSTMCRF model
-    Args:
-        args: config file
+    logs at the end of every epoch 
+    and saves model if validation loss
+    improves.
     """
     sent_vocab = BuildVocab.load(
         os.path.join(args['SENT_VOCAB_PATH'], "sent_vocab.json")
@@ -58,16 +59,12 @@ def train(args, device, writer):
             nn.init.constant_(param.data, 0)
     
     optimizer = torch.optim.Adam(model.parameters(), lr=float(args['lr']))
-    train_iter = 0  # train iter num
-    record_loss_sum, record_tgt_word_sum, record_batch_size = 0, 0, 0  # sum in one training log
-    cum_loss_sum, cum_tgt_word_sum, cum_batch_size = 0, 0, 0  # sum in one validation log
-    record_start, cum_start = time.time(), time.time()
 
     #training loop
     print('start training...')
     for epoch in range(max_epoch):
+        epoch_times =  time.time()
         for sentences, tags in utils.batch_iter(train_data, batch_size=int(args['batch_size'])):
-            train_iter += 1
             current_batch_size = len(sentences)
             sentences, sent_lengths = utils.pad(sentences, sent_vocab[sent_vocab.PAD], device)
             tags, _ = utils.pad(tags, tag_vocab[tag_vocab.PAD], device)
@@ -79,63 +76,22 @@ def train(args, device, writer):
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=float(args['clip_max_norm']))
             optimizer.step()
+        
+        
+        dev_loss = cal_dev_loss(model, dev_data, args['batch_size'], sent_vocab, tag_vocab, device)
 
-            record_loss_sum += batch_loss.sum().item()
-            record_batch_size += current_batch_size
-            record_tgt_word_sum += sum(sent_lengths)
+        writer.add_scalar("Time/train_val_time", time.time() - epoch_times, epoch+1)
+        writer.add_scalar("Loss/Train", loss, epoch+1)
+        writer.add_scalar("Loss/Validation", dev_loss, epoch+1)
 
-            cum_loss_sum += batch_loss.sum().item()
-            cum_batch_size += current_batch_size
-            cum_tgt_word_sum += sum(sent_lengths)
-
-            if train_iter % log_every == 0:
-                print('log: epoch %d, iter %d, %.1f words/sec, avg_loss %f, time %.1f sec' %
-                      (epoch + 1, train_iter, record_tgt_word_sum / (time.time() - record_start),
-                       record_loss_sum / record_batch_size, time.time() - record_start))
-                record_loss_sum, record_batch_size, record_tgt_word_sum = 0, 0, 0
-                record_start = time.time()
-
-            if train_iter % validation_every == 0:
-                print('dev: epoch %d, iter %d, %.1f words/sec, avg_loss %f, time %.1f sec' %
-                      (epoch + 1, train_iter, cum_tgt_word_sum / (time.time() - cum_start),
-                       cum_loss_sum / cum_batch_size, time.time() - cum_start))
-                cum_loss_sum, cum_batch_size, cum_tgt_word_sum = 0, 0, 0
-
-                dev_loss = cal_dev_loss(model, dev_data, args['batch_size'], sent_vocab, tag_vocab, device)
-                if dev_loss < min_dev_loss * float(args['patience_threshold']):
-                    min_dev_loss = dev_loss
-                    #save only if there is improvement in val_loss
-                    model.save(model_save_path)
-                    torch.save(optimizer.state_dict(), optimizer_save_path)
-                    patience = 0
-                else:
-                    patience += 1
-                    if patience == int(args['max_patience']):
-                        decay_num += 1
-                        if decay_num == int(args['max_decay']):
-                            print('Early stop. Save result model to %s' % model_save_path)
-                            return
-                        lr = optimizer.param_groups[0]['lr'] * float(args['lr_decay'])
-                        #TODO: check for faster tweaks here
-                        model = Model.BiLSTMCRF.load(model_save_path, device)
-                        optimizer.load_state_dict(torch.load(optimizer_save_path))
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr
-                        patience = 0
-                print('dev: epoch %d, iter %d, dev_loss %f, patience %d, decay_num %d' %
-                      (epoch + 1, train_iter, dev_loss, patience, decay_num))
-                cum_start = time.time()
-                if train_iter % log_every == 0:
-                    record_start = time.time()
-        #log metrics            
-        writer.add_scalar(
-            'Loss/Train', record_loss_sum, epoch
-        )
-        writer.add_scalar(
-            'Loss/Validation', cum_loss_sum, epoch
-        )
-    print('Reached %d epochs, Save result model to %s' % (max_epoch, model_save_path))
-
+        if dev_loss < min_dev_loss:
+            print(f"Saved new model at epoch : {epoch+1}")
+            min_dev_loss = dev_loss
+            model.save(model_save_path)
+            torch.save(
+                optimizer.state_dict(), optimizer_save_path
+            )
+   
 def test(args, device):
     """ Testing the model
     Args:
